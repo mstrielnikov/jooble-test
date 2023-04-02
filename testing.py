9ccc0dbc4f786ff2b858b3453e0ddbdd06895ec4
mstrielnikov <makstr@ukr.net>
2023-04-02 23:06:01 +0300

    Parametrize feature column headers

--- testing.py
+++ testing.py
@@ -12,7 +12,7 @@ def z_score(x, mean, stddev):
 # Convert function which compute Z-score into UDF
 z_score_udf = udf(z_score, FloatType())
 
-def transform_data(input_test_path, input_train_path, output_path):
+def transform_data(input_test_path, input_train_path, output_path, prefix="feature_type_1"):
     """
     Transform data in CSV format by computing the Z-score for each value in selected columns.
     
@@ -20,20 +20,21 @@ def transform_data(input_test_path, input_train_path, output_path):
         input_test_path (str): Path to the input test CSV file.
         input_train_path (str): Path to the input CSV file with training data.
         output_path (str): Path to the output CSV file.
+        prefix (str): Column header's prefix (pattern) which is about to transform
     """
     # Load *.csv
     train_df = spark.read.csv(input_train_path, header=True, inferSchema=True)
     test_df = spark.read.csv(input_test_path, header=True, inferSchema=True)
 
-    # Enumerate column headers which starts with `feature_type_1_`
-    feature_cols = [ header for header in test_df.columns if header.startswith('feature_type_1_') ]
+    # Enumerate column headers which starts with `feature_type_1`
+    feature_cols = [ header for header in test_df.columns if header.startswith(prefix) ]
 
     # Iterate over feature_type_1_{i} columns and perform z-standardization for each value in a feature column
     for feature_col in feature_cols:
         mean_val = lit(train_df.select(mean(feature_col)).collect()[0][0])
         stddev_val = lit(train_df.select(stddev(feature_col)).collect()[0][0])
 
-        new_column_name = "feature_type_1_stand_{}".format(feature_col.removeprefix('feature_type_1_'))
+        new_column_name = "{}_stand{}".format(prefix, feature_col.removeprefix(prefix))
 
         test_df = test_df.withColumn(feature_col, z_score_udf(col(feature_col), mean_val, stddev_val))\
                          .withColumnRenamed(feature_col, new_column_name)
@@ -49,6 +50,6 @@ if __name__ == "__main__":
     train_data_path = f"{WorkDir}/data/input/train.csv"
     test_data_path = f"{WorkDir}/data/input/test.csv"
     output_data_path = f"{WorkDir}/data/output/test_transformed.csv"
-    transformed = transform_data(test_data_path, train_data_path, output_data_path)
+    transformed = transform_data(test_data_path, train_data_path, output_data_path, prefix="feature_type_1")
     transformed.show()
     spark.stop()
