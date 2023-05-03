fd2e1b7f289fcf113beeaa35e9e25b22148cafba
mstrielnikov <makstr@ukr.net>
2023-05-03 19:27:44 +0300

    Fixed setup

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jooble-test",
+    name="score-standardization-spark",
     version="0.1",
     packages=find_packages(),
     install_requires=[
         "pyspark==3.2.2",
         "pandas==1.3.5",
+        "matplotlib",
+        'jupyter',
     ],
     entry_points={
         "console_scripts": [
-            "jooble-test = jooble_test.main:main",
+            "score-standardization-spark = score-standardization-spark.main:main",
         ]
     },
 )
